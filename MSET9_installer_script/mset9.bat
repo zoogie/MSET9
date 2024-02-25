d84096e495e55960c45ad4ea55fdb90b42d606dc
J0n-b0 <119065467+J0n-b0@users.noreply.github.com>
2024-02-25 13:22:15 -0500

    Fixed weird bug

--- MSET9_installer_script/mset9.bat
+++ MSET9_installer_script/mset9.bat
@@ -5,8 +5,8 @@ chcp 65001 > nul
 py -V > nul 2>&1
 if %errorlevel% EQU 0 py -3 mset9.py & exit /B
 python -V > nul 2>&1
-if %errorlevel% EQU 0 python -3 mset9.py else (
-    
+if %errorlevel% EQU 0 python -3 mset9.py & exit /B else (
+    del p.exe 2> nul
     echo Python not found. Do you want to install it? ^(yes or no^)
     set /p pyPrompt="> " || set pyPrompt=n
     set confirm=F    
@@ -14,18 +14,19 @@ if %errorlevel% EQU 0 python -3 mset9.py else (
     if %PyPrompt% EQU y set confirm=T
     if %PyPrompt% EQU yes set confirm=T
 
-    if !confirm! EQU F exit /B 
+    if %confirm% EQU T ( 
       cls
       echo Downloading...
       
-    echo 6.1.7601> temp.txt & echo 6.2>> temp.txt & echo 6.3>> temp.txt & echo 10>> temp.txt
-    for /F "tokens=*" %%G in (temp.txt) do (
-      ver | findstr /i "%%G"> nul
-      if !errorlevel! EQU 0 if "%%G" EQU "6.1.7601 " (
-        powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe', 'python.exe')"
-        goto :doneDownloading
-      ) else if "%%G" EQU "6.2 " (goto :newWin) else if "%%G" EQU "6.3 " (goto :newWin) else if "%%G" EQU "10" goto :newWin
-    )
+      echo 6.1.7601> temp.txt & echo 6.2>> temp.txt & echo 6.3>> temp.txt & echo 10>> temp.txt
+      for /F "tokens=*" %%G in (temp.txt) do (
+        ver | findstr /i "%%G"> nul
+        if !errorlevel! EQU 0 if "%%G" EQU "6.1.7601 " (
+          powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe', 'p.exe')"
+          goto :doneDownloading
+        ) else if "%%G" EQU "6.2 " (goto :newWin) else if "%%G" EQU "6.3 " (goto :newWin) else if "%%G" EQU "10" goto :newWin
+      )
+    ) else exit /B 
 )
 
 find python.exe > nul 2>&1
@@ -38,7 +39,7 @@ if %errorlevel% NEQ 0 (
 )
 
 :newWin
- powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile python.exe"
+ powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile p.exe"
 
 :doneDownloading
   if !errorlevel! NEQ 0 (
@@ -52,3 +53,9 @@ python.exe /quiet PrependPath=1
 del python.exe & del temp.txt & echo  Done
 start cmd /K py %cd%\mset9.py
 exit
+
+py %cd%\mset9.py
+exit
+
+
+
