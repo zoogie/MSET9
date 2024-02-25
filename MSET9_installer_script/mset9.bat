c6a3135e11e5d409fcbf62daa948f0cc38cfd4a6
J0n-b0 <119065467+J0n-b0@users.noreply.github.com>
2024-02-24 21:37:52 -0500

    Actually uploaded this time

--- MSET9_installer_script/mset9.bat
+++ MSET9_installer_script/mset9.bat
@@ -1,34 +1,54 @@
 @echo off
+setlocal enabledelayedexpansion
 chcp 65001 > nul
 
-py -V 2> nul
-if %errorlevel% EQU 0 py -3 mset9.py
-python -V 2> nul
+py -V > nul 2>&1
+if %errorlevel% EQU 0 py -3 mset9.py & exit /B
+python -V > nul 2>&1
 if %errorlevel% EQU 0 python -3 mset9.py else (
-    echo Python not found. Do you want to install it? (yes or no)
-    set /p pyPrompt="> "
+    
+    echo Python not found. Do you want to install it? ^(yes or no^)
+    set /p pyPrompt="> " || set pyPrompt=n
     set confirm=F    
 
     if %PyPrompt% EQU y set confirm=T
     if %PyPrompt% EQU yes set confirm=T
 
-    if %confirm% EQU T (
+    if !confirm! EQU F exit /B 
       cls
       echo Downloading...
       
-      ver | findstr "6.1" > nul & :: 6.1 = Windows 7
-      if %errorlevel% EQU 0 (
+    echo 6.1.7601> temp.txt & echo 6.2>> temp.txt & echo 6.3>> temp.txt & echo 10>> temp.txt
+    for /F "tokens=*" %%G in (temp.txt) do (
+      ver | findstr /i "%%G"> nul
+      if !errorlevel! EQU 0 if "%%G" EQU "6.1.7601 " (
         powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe', 'python.exe')"
-      ) else (
-        powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile python.exe"
-      )
-
-      echo  Done & echo Installing...
-      python.exe /quiet PrependPath=1
-      del python.exe & echo  Done
-      start cmd /K py %cd%\mset9.py
-      exit
-    ) else (
-      exit
+        goto :doneDownloading
+      ) else if "%%G" EQU "6.2 " (goto :newWin) else if "%%G" EQU "6.3 " (goto :newWin) else if "%%G" EQU "10" goto :newWin
     )
 )
+
+find python.exe > nul 2>&1
+if %errorlevel% NEQ 0 (
+  cls
+  echo Your Windows version is not supported. Please use a computer with at least Windows 7 + SP1 and rerun this script.
+  del temp.txt
+  pause
+  exit /B
+)
+
+:newWin
+ powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile python.exe"
+
+:doneDownloading
+  if !errorlevel! NEQ 0 (
+    echo Error downloading Python. Rerun this file again or install Python manually.
+    pause
+    exit /B
+  )
+      
+echo  Done & echo Installing...
+python.exe /quiet PrependPath=1
+del python.exe & del temp.txt & echo  Done
+start cmd /K py %cd%\mset9.py
+exit
