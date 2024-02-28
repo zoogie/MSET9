108194d9cee38bda4cae11140a4878820d332d05
J0n-b0 <119065467+J0n-b0@users.noreply.github.com>
2024-02-27 20:16:56 -0500

    expanded out conditionals

--- MSET9_installer_script/mset9.bat
+++ MSET9_installer_script/mset9.bat
@@ -3,16 +3,26 @@ setlocal enabledelayedexpansion
 chcp 65001 > nul
 
 py -V > nul 2>&1
-if %errorlevel% EQU 0 py -3 mset9.py & exit /B
+if %errorlevel% EQU 0 (
+  py -3 mset9.py 
+  exit /B
+)
 python -V > nul 2>&1
-if %errorlevel% EQU 0 python mset9.py & exit /B else (
-  del p.exe 2> nul
+if %errorlevel% EQU 0 (
+  python mset9.py
+  exit /B 
+)
+  del p.exe temp.txt 2> nul
   echo Python not found. Do you want to install it? ^(yes or no^)
   set /p pyPrompt="> "
   set confirm=F
 
-  if [%PyPrompt%] EQU [y] set confirm=T
-  if [%PyPrompt%] EQU [yes] set confirm=T
+  if [%PyPrompt%] EQU [y] ( 
+    set confirm=T
+  )
+  if [%PyPrompt%] EQU [yes] (
+    set confirm=T
+  )
 
   if %confirm% EQU T ( 
     cls
@@ -21,27 +31,56 @@ if %errorlevel% EQU 0 python mset9.py & exit /B else (
     echo 6.1.7601> temp.txt & echo 6.2>> temp.txt & echo 6.3>> temp.txt & echo 10>> temp.txt
     for /F "tokens=*" %%G in (temp.txt) do (
       ver | findstr /i "%%G"> nul
-      if !errorlevel! EQU 0 if "%%G" EQU "6.1.7601 " (
-        powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe', 'p.exe')"
-        goto :doneDownloading
-      ) else if "%%G" EQU "6.2 " (goto :newWin) else if "%%G" EQU "6.3 " (goto :newWin) else if "%%G" EQU "10" goto :newWin)) else exit /B)
+      if !errorlevel! EQU 0 (
+        if "%%G" EQU "6.1.7601 " (
+          powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe', 'p.exe')"
+          goto :doneDownloading
+        )
+      ) else (
+        if "%%G" EQU "6.2 " (
+          goto :newWin
+        ) else (
+          if "%%G" EQU "6.3 " (
+            goto :newWin
+          ) else (
+            if "%%G" EQU "10" (
+              goto :newWin
+            )
+          ) 
+        )
+      )
+    ) 
+  ) else (
+    exit /B
+  )
 
 if not exist p.exe (
   cls
   del temp.txt
   echo Your Windows version is not supported. Please use a computer with at least Windows 7 + SP1 and rerun this script.
   pause
-  exit /B)
+  exit /B
+  )
 
 :newWin
- powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile p.exe"
+ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe', 'p.exe')"
 
 :doneDownloading
- del temp.txt
- if !errorlevel! NEQ 0 echo Error downloading Python. Rerun this file again or download and install Python manually. & pause & exit /B    
- echo  Done & echo Installing...
- p.exe /quiet AppendPath=1
- if %errorlevel% NEQ 0 echo Error installing Python. Rerun this file again or install Python manually. & pause & exit /B
- del p.exe & echo  Done
- echo Python installed successfully. Please rerun this script to open MSET9.
- pause & exit
+  if !errorlevel! NEQ 0 (
+    echo Error downloading Python. Rerun this file again or download and install Python manually.
+    pause
+    exit /B    
+  )
+  echo  Done 
+  echo Installing...
+  p.exe /quiet AppendPath=1
+  if %errorlevel% NEQ 0 (
+    echo Error installing Python. Rerun this file again or install Python manually.
+    pause 
+    exit /B
+  )
+  del p.exe temp.txt
+  echo  Done
+  echo Python installed successfully. Please rerun this script to open MSET9.
+  pause 
+  exit
