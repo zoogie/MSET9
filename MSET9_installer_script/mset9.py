d70f52fd2e5803c5c52cf1ebaf350a2136936d28
luigoalma <luigoalma@hotmail.com>
2024-08-16 00:24:08 +0100

    Bump 2.0

--- MSET9_installer_script/mset9.py
+++ MSET9_installer_script/mset9.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import abc, sys, os, platform, time, pathlib, binascii
 
-VERSION = "v1.2c"
+VERSION = "v2.0"
 
 def prgood(content):
 	# print(f"[\033[0;32m✓\033[0m] {content}")
